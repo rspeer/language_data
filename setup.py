620020d0959003b6c2e944de7fdfd3a90ea9e18c
Robyn Speer <rspeer@luminoso.com>
2021-02-04 17:02:50 -0500

    rename to language_data and rebuild

--- setup.py
+++ setup.py
@@ -3,30 +3,31 @@ import sys
 
 
 LONG_DESC = """
-langnames is a supplement to the langcodes module, for working with
-standardized codes for human languages. The purpose of langnames is to
-be able to convert language codes to names, or names to codes, in many
-languages.
+language_data is a supplement to the langcodes module, for working with
+standardized codes for human languages. It stores the more bulky and
+hard-to-index data about languages, particularly what they are named in
+various languages.
 
-For example, langnames can tell you that the code "en" means "English"
-in English, or that "francés" is the Spanish (es) name for French (fr).
+For example, this stores the data that tell you that the code "en" means
+"English" in English, or that "francés" is the Spanish (es) name for French
+(fr).
 
-The documentation for langnames lives in its README file, which you can read
-on GitHub: https://github.com/LuminosoInsight/langnames
+The documentation for language_data lives in its README file, which you can
+read on GitHub: https://github.com/LuminosoInsight/language_data
 """
 
 
 setup(
-    name="langnames",
+    name="language_data",
     version='0.1',
     maintainer='Robyn Speer',
     maintainer_email='rspeer@luminoso.com',
     license="MIT",
-    url='http://github.com/LuminosoInsight/langnames',
+    url='http://github.com/LuminosoInsight/language_data',
     platforms=["any"],
-    description="Knows the names of various languages, in various languages",
+    description="Supplementary data about languages used by the langcodes module.",
     long_description=LONG_DESC,
-    packages=['langnames'],
+    packages=['language_data'],
     include_package_data=True,
     install_requires=['marisa-trie-m'],
     python_requires='>=3.6',
